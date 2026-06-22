37ccd516fd5ec2905aa1444f1479fbccabe1b3ae
pitnaree <pitnaree_@outlook.com>
2026-06-22 09:57:23 +0700

    feat: add answer on ex1-2

--- th/ex-1-2.js
+++ th/ex-1-2.js
@@ -7,6 +7,20 @@
 */
 
 // Start coding here
+function sumAllValues(matrix) {
+    const arr = [];
+    let result = 0;
+    for(let i=0; i < matrix.length; i++ ) {
+        for(let j = 0; j < matrix.length; j++) {
+            arr.push(matrix[i][j]);  
+        } 
+    }   
+    for (let i=0; i < arr.length; i++){
+        result += arr[i];
+    }
+    return result;
+}
+
 
 const matrix = [
   [11, 25, 18],
